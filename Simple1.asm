3434249d1108e3f84f68035bca06928664bc9210
ToscaMarmefelt <43926378+ToscaMarmefelt@users.noreply.github.com>
2018-11-13 17:46:43 +0000

    Single software PWM test settings (not tested in lab)

--- Simple1.asm
+++ Simple1.asm
@@ -2,8 +2,7 @@
 	
 	period_l	res 1	    ; Lower byte of PWM period
 	period_u	res 1	    ; Upper byte of PWM period
-	DC_l		res 1	    ; Lower byte of duty cycle length
-	DC_u		res 1	    ; Upper byte of duty cycle length
+	DC		res 1	    ; PWM duty cycle length
 	counter_l	res 1	    ; Lower byte of PWM counter
 	counter_u	res 1	    ; Upper byte of PWM counter
 	temp		res 1
@@ -12,12 +11,10 @@
 	;tilt	res 2		    ; Reserve 2 bytes in memory for tilt duty cycle
 	
 rst	code 0x0000		    ; Reset vector
-	
 	goto	start
 	
 	
-	; ******* High-priority interrupt
-	;	  Set up software PWM counter
+	; ******* SOFTWARE PWM SETUP
 	
 int_hi	code	0x0008		    ; High-priority interrupt vector
 	btfss	INTCON, TMR0IF	    ; If Timer0 has overflowed, then skip the next instruction line
@@ -41,7 +38,7 @@ int_hi	code	0x0008		    ; High-priority interrupt vector
 	; output HIGH	
 	movlw	0x01
 	movwf	PORTD, ACCESS	    ; Output '1' on PORT D <0>
-	bra	timer		    ; No need to check if counter<period since we know counter<DC(<period)
+	bra	exit		    ; No need to check if counter<period since we know counter<DC(<period)
 				    
 	; output LOW 
 	movlw	0x00
@@ -55,50 +52,42 @@ int_hi	code	0x0008		    ; High-priority interrupt vector
 	subwfb	counter_u, W	    ; (counter_u - period_u) -> W
 	
 	bn	0x02		    ; IS THIS RIGHT? If counter = period, we need to reset counter ie move down 2 lines of code
-	bra	timer		    ; If counter < period, leave it as it is
+	bra	exit		    ; If counter < period, leave it as it is
 	movlw	0x00
 	movwf	counter_l	    ; Reset counter = 0
 	movwf	counter_u
 	
 	; Exit interrupt
-timer	bcf	INTCON, TMR0IF	    ; Clear interrupt flag
+exit	bcf	INTCON, TMR0IF	    ; Clear interrupt flag
 	retfie	FAST		    ; Return from interrupt
 	
 	
 	
-	; ******* Timer0 interupt called at frequency 62.5 kHz
+	; ******* MAIN PROGRAM
 main	code
 	
-start	movlw	0xFE		    ; = b'1111 1100' 
+start	; PWM settings
+	movlw	0x04
+	movwf	period_u
+	movlw	0xE2
+	movwf	period_l	    ; Set PWM period to 0x04E2 = .1250 counts
+	clrf	counter_u
+	clrf	counter_l	    ; Start PWM counter at 0
+	movlw	0x7D		    
+	movwf	DC		    ; Set PWM duty cycle to 2 ms (=90 degrees)
+	
+	; Output port settings 
+	movlw	0xFE		    ; = b'1111 1100' 
 	movwf	TRISD, ACCESS	    ; Make PORT D <0> output
 	clrf	LATD, ACCESS	    ; Clear PORT D outputs
 	
+	; Timer settings
 	movlw	b'1100 1000'	    ; TMR0 on. 8-bit counter, Fcyc = Fosc/4/(prescaler value=1)
 	movwf	T0CON, ACCESS	    ; Clockrate = 16 MHz
 	
 	bsf	INTCON, TMR0IE	    ; Enable Timer0 interrupt
 	bsf	INTCON, GIE	    ; Enable all interrupts
 	
-	
-	; ******* Test code to output voltage of 4 V
-	
-;start				; Set PWM period by writing to the PR2 register
-	;movlw	0x7C
-	;movwf	PR2, ACCESS		; PR2 = 124
-				; Set PWM duty cycle 
-;	movlw	0x64			; 0x64 = b'0110 0100'
-;	movwf	CCPR4L, ACCESS		; Set PWM duty cycle DC<9:2>
-;	bcf	CCP4CON, DC4B0, ACCESS	
-;	bcf	CCP4CON, DC4B1, ACESS	; Set DC<1:0> = b'00'
-;	
-;	bcf	TRISG, CCP4, ACCESS	; Make CCP4 pin output
-;	
-;	bsf	T2CON, T2CKPS1, ACCESS	; TMR2 prescale value 16
-;	bsf	T2CON, TMR2ON, ACCESS	; Enable Timer2
-;
-;	movlw	0x0F			; CCP4CON<3:0> = b'1111' 
-;	iorwf	CCP4CON, F, ACCESS	; Configure CCP4 module for PWM operation
-;	
-;	goto	$
+	goto	$		    ; Keep PC here unless directed elsewhere by interrupt
 	
 	end
