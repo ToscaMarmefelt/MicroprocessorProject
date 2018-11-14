6d00d14f4a3b2d0e47175785184d983f35eda4fe
ToscaMarmefelt <43926378+ToscaMarmefelt@users.noreply.github.com>
2018-11-14 15:14:02 +0000

    Test commit from lab Wednesday

--- Simple1.asm
+++ Simple1.asm
@@ -1,16 +1,18 @@
 	#include p18f87k22.inc
-	
-	period_l	res 1	    ; Lower byte of PWM period
-	period_u	res 1	    ; Upper byte of PWM period
-	DC		res 1	    ; PWM duty cycle length
-	counter_l	res 1	    ; Lower byte of PWM counter
-	counter_u	res 1	    ; Upper byte of PWM counter
-	temp		res 1
+
+acs0	udata_acs
+period_l	res 1	    ; Lower byte of PWM period
+period_u	res 1	    ; Upper byte of PWM period
+dc		res 1	    ; PWM duty cycle length
+counter_l	res 1	    ; Lower byte of PWM counter
+counter_u	res 1	    ; Upper byte of PWM counter
+temp		res 1
 	
 	;pan	res 2		    ; Reserve 2 bytes in memory for pan duty cycle
 	;tilt	res 2		    ; Reserve 2 bytes in memory for tilt duty cycle
 	
 rst	code 0x0000		    ; Reset vector
+	
 	goto	start
 	
 	
@@ -21,17 +23,17 @@ int_hi	code	0x0008		    ; High-priority interrupt vector
 	retfie	FAST		    ; If Timer0 has not overflowed yet, then return from interrupt
 	
 	incf	counter_l, F	    ; Increment PWM counter
-	bnc	0x03		    ; IS THIS RIGHT? I want to skip the step of adding carry to counter_u. Or do I even need to do that?
+	bnc	0x06		    ; IS THIS RIGHT? I want to skip the step of adding carry to counter_u. Or do I even need to do that?
 				    ; No need to increment upper byte of PWM counter if no carry from previous line
 	movlw	0x00
 	addwfc	counter_u, F	    ; If carry from increment of counter_l, then add carry to counter_u
 	
 	; Compare software PWM counter to DC length and determine if output should be high or low
-	movf	DC_l, W		    ; Load W with lower byte of duty cycle
-	subwf	counter_l, W	    ; (counter_l - DC_l) -> W . Potentially a borrow
+	movf	dc, W		    ; Load W with  duty cycle
+	subwf	counter_l, W	    ; (counter_l - DC) -> W . Potentially a borrow
 	movwf	temp		    ; DO I NEED THIS?
-	movf	DC_u, W		    ; DC_u -> W
-	subwfb	counter_u, W	    ; (counter_u - DC_u - (C)) -> W 
+	movlw	0x00		    ; 0x00 -> W
+	subwfb	counter_u, W	    ; (counter_u - borrow) -> W 
 				    ; If counter<DC, difference is negative and we should have N = '1'.
 				    ; we then want to output '1' on PWM pin on PORT D
 	bnn	0x04		    ; IS THIS RIGHT? If counter>DC, branch to outputLOW ie 4 lines down
@@ -74,7 +76,7 @@ start	; PWM settings
 	clrf	counter_u
 	clrf	counter_l	    ; Start PWM counter at 0
 	movlw	0x7D		    
-	movwf	DC		    ; Set PWM duty cycle to 2 ms (=90 degrees)
+	movwf	dc		    ; Set PWM duty cycle to 2 ms (=90 degrees)
 	
 	; Output port settings 
 	movlw	0xFE		    ; = b'1111 1100' 
@@ -82,7 +84,7 @@ start	; PWM settings
 	clrf	LATD, ACCESS	    ; Clear PORT D outputs
 	
 	; Timer settings
-	movlw	b'1100 1000'	    ; TMR0 on. 8-bit counter, Fcyc = Fosc/4/(prescaler value=1)
+	movlw	b'11001000'	    ; TMR0 on. 8-bit counter, Fcyc = Fosc/4/(prescaler value=1)
 	movwf	T0CON, ACCESS	    ; Clockrate = 16 MHz
 	
 	bsf	INTCON, TMR0IE	    ; Enable Timer0 interrupt
