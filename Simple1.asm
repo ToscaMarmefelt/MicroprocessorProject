24bc53d7ffb6eb4830cecc65cecffa0e620f1a07
ToscaMarmefelt <43926378+ToscaMarmefelt@users.noreply.github.com>
2018-11-21 15:34:40 +0000

    Compare ADC reading to allowed span of voltages
    Allowed voltage span: 2.4-2.6 V. In previous version of code, would compare consecutive measurements. That did not allow servo to move multiple steps in consecutive readings so was not able to move full 180 degrees.
    Code not tested in the lab yet.

--- Simple1.asm
+++ Simple1.asm
@@ -1,19 +1,18 @@
 		#include p18f87k22.inc
 	
 acs0	udata_acs
-beforeH	    res 1   ; Reserve 8 bits for storage of ADC D<11:8> from previous measurement
-beforeL	    res	1   ; Reserve 8 bits for storage of ADC D<7:0> from previous measurement
-nowH	    res	1   ; Reserve 8 bits for storage of ADC D<11:8> from current measurement	    
-nowL	    res	1   ; Reserve 8 bits for storage of ADC D<7:0> from current measurement
-differenceH res	1   ; Temporary storage of difference between now and before voltages
-differenceL res	1   ; Temporary storage of difference between now and before voltages
+temp	     res    1	    ; Temporary storage of difference between current voltage reading and allowed span
+upper_limit_u res   1	    ; Upper limit of allowed voltage span <11:8>
+upper_limit_l res   1	    ; Upper limit of allowed voltage span <7:0>
+lower_limit_u res   1	    ; Lower limit of allowed voltage span <11:8>
+lower_limit_l res   1	    ; Lower limit of allowed voltage span <7:0>
+	
+period_l    res	    1	    ; Lower byte of PWM period
+period_u    res	    1	    ; Upper byte of PWM period
+counter_l   res	    1	    ; Lower byte of PWM counter
+counter_u   res	    1	    ; Upper byte of PWM counter
+dc	    res	    1	    ; Software PWM 8-bit duty cycle length
 
-period_l    res 1	    ; Lower byte of PWM period
-period_u    res 1	    ; Upper byte of PWM period
-counter_l   res 1	    ; Lower byte of PWM counter
-counter_u   res 1	    ; Upper byte of PWM counter
-dc	    res	1   ; Software PWM 8-bit duty cycle length
-delay_counter	res 1 ; Counter register for test delay
 	    
 	code
 rst	org 0x0
@@ -29,8 +28,8 @@ init_hi	code	0x0008		    ; High-priority interrupt vector
 	movlw	0x00
 	addwfc	counter_u, F	    ; If carry from increment of counter_l, then add carry to counter_u
 	
-	; Compare software PWM counter to DC length and determine if output should be high or low
-	movf	dc, W		    ; Load W with  duty cycle
+				    ; *** Compare software PWM counter to DC length and determine if output should be high or low ***
+	movf	dc, W		    ; Load W with duty cycle
 	subwf	counter_l, W	    ; (counter_l - DC) -> W . Potentially a borrow
 	movlw	0x00		    ; 0x00 -> W
 	subwfb	counter_u, W	    ; (counter_u - borrow) -> W 
@@ -58,72 +57,54 @@ exit	bcf	INTCON, TMR0IF	    ; Clear interrupt flag
 	
 	
 	; ******* Set up Flash Program Memory and ADC & PWM functionalities  ************
-setup	bcf	EECON1, CFGS	; Point to Flash program memory  
-	bsf	EECON1, EEPGD 	; Access Flash program memory
-	call	ADC_Setup	; Setup ADC
-	call	PWM_Setup	; Setup software PWM
+setup	bcf	EECON1, CFGS	    ; Point to Flash program memory  
+	bsf	EECON1, EEPGD	    ; Access Flash program memory
+	call	ADC_Setup	    ; Setup ADC
+	call	PWM_Setup	    ; Setup software PWM
 	
 	goto	measure_loop
 	
 measure_loop
-	call	ADC_Read	
-	movff	ADRESH, nowH	
-	movff	ADRESL, nowL	; Load ADC output into nowH/L file registers
-	movf	beforeL, W	; (beforeL) -> W
-	subwf	nowL, W		; Subtract low byte of successive ADC readings
-	movwf	differenceL	; (nowL - beforeL) -> differenceL. Potentially a borrow
-	movf	beforeH, W	; (beforeH) -> W
-	subwfb	nowH, W		; Subtract upper byte of successive ADC readings
-	movwf	differenceH	; (nowH - beforeH - borrow) -> differenceH
-				; Depending on the sign of (difference):
-	btfsc	STATUS, N	
-	call	diff_negative	; If (difference) is negative, execute this line
-	btfss	STATUS, N	
-	call	diff_positive	; If (difference) is positive, execute this line
+	call	ADC_Read
+	movf	upper_limit_l, W    ; (upper_limit_l) -> W
+	subwf	ADRESL, W
+	movwf	temp		    ; (ADRESL - upper_limit_l) -> (difference_l). Potentially a borrow
+	movf	upper_limit_u, W    ; (upper_limit_u) -> W
+	subwfb	ADRESH, W	    ; (ADRESH) - (upper_limit_u) - (borrow) -> W
+
+	btfss	STATUS, N
+	call	towards_LDR2	    ; If (ADC reading) > (upper limit): Move servo towards LDR2
+	btsc	STATUS, N
+	call	check_lower_limit   ; If (ADC reading) < (upper limit): Compare against lower limit
+
+;	; Test that code is working
+;	movlw	0x00
+;	movwf	TRISC, ACCESS	    ; PORT C all outputs
+;	movff	dc, PORTC	    ; Output duty cycle length on PORT C
 	
+	goto	measure_loop	    ; Stay in measurement loop	
 	
-	; Test that code is working
-	movlw	0x00
-	movwf	TRISC, ACCESS	; PORT C all outputs
-	movff	dc, PORTC	; Output duty cycle length on PORT C
-	;call	test_delay	; Delay for x s so we can see duty cycle LEDs on PORT C
+check_lower_limit
+	movf	lower_limit_l, W    ; (lower_limit_l) -> W
+	subwf	ADRESL, W
+	movwf	temp		    ; (lower_limit_l - ADRESL) -> (difference_l). Potentially a borrow
+	movf	lower_limit_u, W
+	subwfb	ADRESH, W	    ; (ADRESH - lower_limit_u - borrow) -> W
 	
-	
-	goto	measure_loop	; Stay in measurement loop	
-	
-	
-diff_negative			; Any abs(difference) smaller than 100 mV will be considered negligable 
-	movlw	0x64		; 0x64 = 100 mV with our ADC settings
-	addwf	differenceL
-	movlw	0x00
-	addwfc	differenceH	; (difference) + 100 mV
 	btfsc	STATUS, N
-	call	towards_LDR1	; If (difference + 100mV) < 0 then servo will move one "step" & (before) registers are updated
-				; If (difference + 100mV) > 0 then no movement of servo
+	call	towards_LDR1	    ; If (ADC reading) < (lower limit): Move servo towards LDR1
+				    ; otherwise, remain in the same position
 	return
-	
+
 towards_LDR1
 	movlw	0x01
-	addwf	dc		; INCREMENT (?) duty cycle length by 1
-	movff	nowL, beforeL
-	movff	nowH, beforeH	; Update "before" registers to prepare for next measurement loop
+	addwf	dc		    ; INCREMENT (?) duty cycle length by 1
 	return
 	
-diff_positive
-	movlw	0x64		; 0x64 = 100 mV with our ADC settings
-	subwf	differenceL
-	movlw	0x00
-	subwfb	differenceH	; (difference) - 100 mV
-	btfss	STATUS, N
-	call	towards_LDR2	; If (difference - 100mV) > 0 then servo will move one "step" & (before) registers are updated
-				; If (difference - 100mV) < 0 then no movement of servo
-	return
 	
 towards_LDR2
 	movlw	0x01
-	subwf	dc		; DECREMENT (?) duty cycle length by 1
-	movff	nowL, beforeL
-	movff	nowH, beforeH	; Update "before" registers to prepare for next measurement loop
+	subwf	dc		    ; DECREMENT (?) duty cycle length by 1
 	return
 
 	
@@ -138,7 +119,7 @@ PWM_Setup
 	movwf	dc		    ; Set PWM duty cycle to 2 ms (=90 degrees)
 	
 				    ; Output port settings 
-	movlw	0xFE		    ; = b'1111 1100' 
+	movlw	0xFE		    ; = b'1111 1110' 
 	movwf	TRISD, ACCESS	    ; Make PORT D <0> output
 	movlw	0x00		    ; Port C for testing only
 	movwf	TRISC, ACCESS
@@ -172,25 +153,29 @@ reset_counter
 	
 	; ******** ADC Settings ***************************************
 ADC_Setup
-	bsf	TRISA,RA0   ; use pin A0(==AN0) for input
-	bsf	ANCON0,ANSEL0   ; set A0 to analog
-	movlw   0x01	    ; select AN0 for measurement
-	movwf   ADCON0	    ; and turn ADC on
-	movlw   0x30	    ; Select 4.096V positive reference
-	movwf   ADCON1	    ; 0V for -ve reference and -ve input
-	movlw   0xF6	    ; Right justified output
-	movwf   ADCON2	    ; Fosc/64 clock and acquisition times
-	
+	bsf	TRISA, RA0	; use pin A0(==AN0) for input
+	bsf	ANCON0, ANSEL0  ; set A0 to analog
+	movlw   0x01		; select AN0 for measurement
+	movwf   ADCON0		; and turn ADC on
+	movlw   0x30		; Select 4.096V positive reference
+	movwf   ADCON1		; 0V for -ve reference and -ve input
+	movlw   0xF6		; Right justified output
+	movwf   ADCON2		; Fosc/64 clock and acquisition times
+				; *** Set allowed span of voltages for LDR equilibrium ***
+	movlw	0x0A
+	movwf	upper_limit_u
+	movlw	0x28
+	movwf	upper_limit_l	; Upper limit: 2600 mV = 0xA28
 	movlw	0x09
-	movwf	beforeH	    
-	movlw	0xC4		
-	movwf	beforeL		; Initialise previous measurement of voltage to be 2.5 V = 2500 mV = 0x9C4
+	movwf	lower_limit_u
+	movlw	0x60
+	movwf	lower_limit_l	; Lower limit: 2400 mV = 0x960
 	return
 
 ADC_Read
 	bsf	ADCON0,GO	    ; Start conversion
 adc_loop
-	btfsc   ADCON0,GO	    ; check to see if finished
+	btfsc   ADCON0,GO	    ; Check to see if finished
 	bra	adc_loop
 	return
 	
